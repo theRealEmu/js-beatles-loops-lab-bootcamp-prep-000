af99102f7c0e50429c9f5ff8a4e943886202101e
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 11:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+
+
+//1 Create a function theBeatlesPlay, which accepts two parameters--an array of musicians and an array of instruments.
+function theBeatlesPlay(musiciansArr, instrumentsArr) {
+  
+  var arr = [];
+}
