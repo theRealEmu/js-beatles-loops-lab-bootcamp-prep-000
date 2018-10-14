174999e18309654a9a0e04df16466a8e46a67d2d
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 11:35:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
 //1 Create a function theBeatlesPlay, which accepts two parameters--an array of musicians and an array of instruments.
 function theBeatlesPlay(musiciansArr, instrumentsArr) {
   
-  var arr = [];
+  var musiciansAndInstruments = [];
   
   for (let i = 0; i < musiciansArr.length; i += 1) {
     
     var str = `${musiciansArr[i]} plays ${instrumentsArr[i]}`;
-    Array.concat(arr, str);
+   musiciansAndInstruments.push(str);
     
   }
   
-  return arr;
+  return musiciansAndInstruments;
 }
