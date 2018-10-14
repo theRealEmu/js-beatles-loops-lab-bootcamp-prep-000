7ed20b44253b94a9364fcaea4bee7c988333f1dc
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 11:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,4 +46,19 @@ function johnLennonFacts(factsArr) {
 }
 
 
-//
+//Create a function iLoveTheBeatles which accepts a number as a parameter.
+//The function should create a variable that stores an empty array.
+//Implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array.
+//The loop should then increment the number passed in as a parameter. The condition of the loop should check to see that the parameter number is less than 15. It should contain a number of strings that represents the difference between the input number and 15.
+Return the array with the strings "I love the Beatles!".
+
+function iLoveTheBeatles(num) {
+  
+  var arr = [];
+  
+  do {
+    arr.push("I love the beatles!");
+    num += 1;
+  } while(num < 15);
+  
+}
