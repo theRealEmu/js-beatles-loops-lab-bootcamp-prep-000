729cabf1cfe99a9e2da91e214ca5d56d276e6a96
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 11:42:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ function theBeatlesPlay(musiciansArr, instrumentsArr) {
 
 //Create a function johnLennonFacts.
 //This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 //Use a while loop to loop over the facts array and add "!!!" to the end of every fact.
 
 function johnLennonFacts(factsArr) {
